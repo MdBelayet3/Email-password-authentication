ddd7ef46535317753e9b9025e2054610861f96e1
Md belayet <mdbelayethossain016@gmail.com>
2025-05-03 10:28:56 +0600

    project complete

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,118 @@
-import React from 'react';
+import { sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
+import auth from '../../Firebase/Firebase.init';
+import { Link } from 'react-router';
 
 const Login = () => {
+
+    // State
+    const [loginError, setLoginError] = useState('');
+    const [loginSuccess, setLoginSuccess] = useState('');
+    const emailRef = useRef(null);
+
+    // handleLogin function
+    const handleLogin = e => {
+        e.preventDefault();
+
+        // input field
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+        // remove previous error and success
+        setLoginError('');
+        setLoginSuccess('');
+
+        // Sign in with emil and password method
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+                if (result.user.emailVerified) {
+                    setLoginSuccess("Login has completed successfully")
+                }
+                else {
+                    sendEmailVerification(result.user)
+                    .then(() =>alert("Please Verified Your Email address"),email)
+                    alert("Please Verified Your Email address"),email
+                    
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                if (error.message === "Firebase: Error (auth/invalid-credential).") {
+                    setLoginError("Wrong Password You have to give correct password")
+                }
+            })
+    }
+
+    // Handle forget password function
+    const handleForgetPassword = () => {
+        const email = emailRef.current.value;
+        if (!email) {
+            alert("Please provide a Email")
+            return;
+        }
+        else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+            alert("Please provide a valid email")
+            return;
+        }
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                alert("Please check you email")
+            })
+            .catch(error => {
+                console.error(error);
+            })
+        console.log("reset email btn", emailRef.current.value)
+    }
+
     return (
         <div>
-            <h2 className="text-4xl">Login Now</h2>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    ref={emailRef}
+                                    type="email"
+                                    placeholder="email"
+                                    name='email'
+                                    className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="password"
+                                    name='password'
+                                    className="input input-bordered" required />
+                                <label className="label">
+                                    <a onClick={handleForgetPassword} className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                        {
+                            loginError && <h2 className='text-red-700 mb-4 ml-3'>{loginError}</h2>
+                        }
+                        {
+                            loginSuccess && <h2 className='text-green-700 mb-4 ml-3'>{loginSuccess}</h2>
+                        }
+                        <p className='ml-4 mb-4'>New in this Website Please <Link className='btn btn-secondary' to="/register">Register</Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,8 +1,9 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
 import auth from '../../Firebase/Firebase.init';
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
+import { Link } from 'react-router';
 
 
 const Register = () => {
@@ -21,11 +22,14 @@ const Register = () => {
         setRegisterSuccess('');
 
         //input field
-        const email = e.target.email.value
-        const password = e.target.password.value
-        console.log(email, password)
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const photoUrl = e.target.file[0];
+        const password = e.target.password.value;
+        const accepted = e.target.terms.checked;
+        console.log(name,email,photoUrl, password, accepted)
 
-        // password validation
+        // password and another validation
         if (password.length < 6) {
             setRegisterError('Password should be at least 6 characters ');
             return;
@@ -34,12 +38,28 @@ const Register = () => {
             setRegisterError("Password should be at least 1 Upper case letter")
             return;
         }
+        else if(!accepted){
+            setRegisterError("Please accept Our terms and conditions")
+            return;
+        }
 
         // create user with Email and Password
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 console.log(result.user);
                 setRegisterSuccess('User successfully created')
+
+                // Profile update
+                updateProfile(result.user,{
+                    displayName: name,
+                    
+                })
+
+                // Email verification
+                sendEmailVerification(result.user)
+                .then(() =>{
+                    alert("Please check your email")
+                })
             })
             .catch(error => {
                 console.log(error.code);
@@ -53,6 +73,8 @@ const Register = () => {
             <div className='mx-auto md:w-1/2'>
                 <h2 className="text-4xl mb-8 ">Please Register</h2>
                 <form onSubmit={handleRegister}>
+                    <input className='bg-slate-300 w-3/4 rounded-md mb-4 py-2 px-4' type="text" placeholder='Your Name' name="name" id="" required /><br />
+                    <input className='bg-slate-300 w-3/4 rounded-md mb-4 py-2 px-4' type="file" name="file" id="" /><br />
                     <input className='bg-slate-300 w-3/4 rounded-md mb-4 py-2 px-4' type="email" placeholder='Email Address' name="email" id="" required /><br />
                     {/* password input field and add password hide and show btn */}
                     <div className='relative'>
@@ -65,6 +87,11 @@ const Register = () => {
                             {toggleEyeBtn?<IoEyeOff className='text-3xl' /> :<IoEye className='text-3xl' />}
                         </span>
                     </div><br />
+                    <div>
+                        <input className='w-4 h-4 mr-4' type="checkbox" name="terms" id="terms" />
+                        <label  className='text-2xl' htmlFor="terms">Accept Our <a href="">terms and Conditions</a></label>
+                    </div>
+                    <br />
                     <input className='btn btn-secondary w-3/4 py-2 px-4' type="submit" value="Register" />
                 </form>
                 {
@@ -73,6 +100,7 @@ const Register = () => {
                 {
                     registerSuccess && <h2 className='text-green-700 mt-5'>{registerSuccess}</h2>
                 }
+                <p className=' mt-4'>If you have any account Please <Link className='btn btn-primary' to="/login">Login</Link></p>
             </div>
         </div>
     );
